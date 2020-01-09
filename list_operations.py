cf010c956021517cb06281a6b3c989c37a38d20a
Your Name <you@example.com>
2020-01-09 12:47:29 -0800

    Finished List Slicing Assignment

--- list_operations.py
+++ list_operations.py
@@ -29,7 +29,7 @@ def head(input_list):
 
     """
 
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -42,7 +42,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -55,7 +55,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
 
 
 def init(input_list):
@@ -68,7 +68,7 @@ def init(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 ##############################################################################
@@ -84,7 +84,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -97,7 +97,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +110,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -123,7 +123,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -139,7 +139,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -154,7 +154,9 @@ def replace_head(input_list):
 
     """
 
-    pass
+    input_list[0] = 42
+
+    return 
 
 
 def replace_third_and_last(input_list):
@@ -168,6 +170,9 @@ def replace_third_and_last(input_list):
     True
 
     """
+
+    input_list[2] = 37
+    input_list[-1] = 37
 
     pass
 
@@ -188,6 +193,8 @@ def replace_middle(input_list):
 
     """
 
+    input_list[2:-2] = [42, 37]
+
     pass
 
 
@@ -202,6 +209,9 @@ def delete_third_and_seventh(input_list):
     True
 
     """
+
+    del input_list[2]
+    del input_list[5]
 
     pass
 
@@ -219,6 +229,8 @@ def delete_middle(input_list):
     True
 
     """
+
+    input_list[2:-2] = []
 
     pass
 
